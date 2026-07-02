2a3d6b32fcff310e0415733d2f02150570924116
dwitio123 <tiothebook@gmail.com>
2026-07-03 06:14:45 +0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Developer dev = new Developer();
+dev.Name = "Prabowo";
+dev.Experience = 2;
+dev.Introduce();
+
+Developer dev2 = new Developer();
+dev2.Name = "Gibran";
+dev2.Experience = 2;
+dev2.Introduce();
+
+public class Developer
+{
+    public string Name { get; set; } = "";
+    public int Experience { get; set; }
+    public void Introduce()
+    {
+        Console.WriteLine($"Halo, saya {Name} dengan pengalaman {Experience} tahun");
+    }
+}
