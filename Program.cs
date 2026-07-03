ce215364a33009464f6bbace1f6142fc0ee58ffd
dwitio123 <tiothebook@gmail.com>
2026-07-04 06:03:26 +0700

    feat: learn data type, var, nullable

--- Program.cs
+++ Program.cs
@@ -8,6 +8,38 @@ dev2.Name = "Gibran";
 dev2.Experience = 2;
 dev2.Introduce();
 
+// -------------------------------------------
+
+Employee employee = new()
+{
+    Name = "Jokowi",
+    Age = 60,
+    isActive = true
+};
+
+Console.WriteLine("-------------------------");
+Console.WriteLine(employee.Name);
+Console.WriteLine(employee.Age);
+Console.WriteLine(employee.isActive);
+Console.WriteLine("-------------------------");
+
+// --------------------------------------------
+
+Laptop laptop = new()
+{
+    Brand = "Lenovo",
+    Processor = "Intel i5",
+    Ram = 16,
+    Price = 8500000
+};
+
+Console.WriteLine("-------------------------");
+Console.WriteLine(laptop.Brand);
+Console.WriteLine(laptop.Processor);
+Console.WriteLine($"{laptop.Ram} GB");
+Console.WriteLine($"Rp{laptop.Price}");
+Console.WriteLine("-------------------------");
+
 public class Developer
 {
     public string Name { get; set; } = "";
@@ -16,4 +48,22 @@ public class Developer
     {
         Console.WriteLine($"Halo, saya {Name} dengan pengalaman {Experience} tahun");
     }
+}
+
+public class Employee
+{
+    public required string Name { get; set; }
+    public int Age { get; set; }
+    public bool isActive { get; set; }
+}
+
+// --------------------------------------------
+// Challenge
+
+public class Laptop
+{
+    public required string Brand { get; set; }
+    public required string Processor { get; set; }
+    public int Ram { get; set; }
+    public double Price { get; set; }
 }
